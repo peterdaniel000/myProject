19afc87e9eeced3af279c9c3b89a89163bfa06ec
peterdaniel000 <peterdanielfun24@gmail.com>
2026-07-18 13:44:33 -0400

    python script to change mac address

--- pythonProject/mac_changer/mac_changer.py
+++ pythonProject/mac_changer/mac_changer.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:22:11:44:11:22", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
