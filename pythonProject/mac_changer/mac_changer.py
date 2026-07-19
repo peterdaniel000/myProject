8765bc3733fd45bdc458c678681a928bedfaa1f1
peterdaniel000 <peterdanielfun24@gmail.com>
2026-07-19 14:46:31 -0400

    making the program more secure

--- pythonProject/mac_changer/mac_changer.py
+++ pythonProject/mac_changer/mac_changer.py
@@ -7,6 +7,10 @@ new_mac = input ("new MAC > ")
 
 print("[+] changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " +new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+#subprocess.call("ifconfig " + interface + " down", shell=True)
+#subprocess.call("ifconfig " + interface + " hw ether " +new_mac, shell=True)
+#subprocess.call("ifconfig " + interface + " up", shell=True)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
