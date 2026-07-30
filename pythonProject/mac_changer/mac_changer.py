ad9c0e1722361ecadca1fa1f227774b9a69d1a37
peterdaniel000 <peterdanielfun24@gmail.com>
2026-07-30 19:01:33 -0400

    complete program

--- pythonProject/mac_changer/mac_changer.py
+++ pythonProject/mac_changer/mac_changer.py
@@ -27,23 +27,23 @@ def change_mac(interface, new_mac):
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-#def get_current_mac(interface):
-    #ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    #print(ifconfig_result)
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
 
-    #mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
-    #if mac_address_result:
-        #print(mac_address_result.group(0))
-    #else:
-        #print("[-] could not find mac address")
+    mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_result:
+       return mac_address_result.group(0)
+    else:
+        print("[-] could not find mac address")
 options = get_arguments()
-#change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+print("current mac = " + str(current_mac))
 
-ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-print(ifconfig_result)
+change_mac(options.interface, options.new_mac)
 
-mac_address_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
-if mac_address_result:
-    print(mac_address_result.group(0))
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print("[+] MAC was successfully changed to " + current_mac)
 else:
-    print("[-] could not find mac address")
+    print("[-] MAC address did not get changed.")
+
