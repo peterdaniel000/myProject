99734f1ed67a214e8052a583ba8de3878518bcf6
root <root@kali.kali>
2026-08-01 18:09:41 -0400

    python for checking ifconfig on linux

--- pythonProject/Network_scanner/network_scanner.py
+++ pythonProject/Network_scanner/network_scanner.py
@@ -4,5 +4,9 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    arp_request.show()
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
+    broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 scan("192.168.12.2/24")
