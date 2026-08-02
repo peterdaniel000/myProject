7754664152c816152767a77b804447e5abc82087
root <root@kali.kali>
2026-08-02 17:35:10 -0400

    sending packet

--- pythonProject/Network_scanner/network_scanner.py
+++ pythonProject/Network_scanner/network_scanner.py
@@ -4,9 +4,12 @@ import scapy.all as scapy
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
+    
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
-    broadcast.show()
+    
     arp_request_broadcast = broadcast/arp_request
-    arp_request_broadcast.show()
+
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
+    print(unanswered.summary())
 scan("192.168.12.2/24")
